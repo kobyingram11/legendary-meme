1cffed9ee510d1066246b515a9ff7b5b5383c307
Koby Ingram <85536932+kobyingram11@users.noreply.github.com>
2022-02-17 11:10:02 -0600

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,40 +4,61 @@
 // .ready( ... ) = argument/parameter passed to that method
 // function(){} in the method is an IIFE = Immediately Invoked F/n Expresson
 $(document).ready(function(){ 
-  //alert("page is ready");
   console.log("doc is ready");
 
-let wowCount = 0;
-let wowMsg;
+  $("#btnGetFacts").click(function (){
 
-
-$( "#dogeDiv" ).draggable(
-
-  {
-    containment: "#dogeHouse", 
-    scroll:false,
-    start: function() {   //start
-      wowCount++;
-      console.log("we're starting.", "this is wowCount", wowCount);
-
-    if (wowCount <= 5){
-      wowMsg = "slight wow. ";
-    }
-    else if(wowCount <=10){
-      wowMsg = "halfwow there. ";
-    }
-    else{
-      wowMsg = "too much wow.";
-    }
+    //if (typeof userName !== 'string'){
+      //let coolFacts='not string';
+       //return coolFacts;
+    //}
   
+      let userName = $("#userName").val();
+      let coolFacts = generateCoolNameFacts(userName);
+      
 
-      $("#wowOutput").text(wowMsg +"you has " + wowCount +  " wow");
-    
-    }//start
+        $("#coolNameFactsOutput").html(coolFacts);
+        
+      });
+
 });
 
+function generateCoolNameFacts(userName){
+  let coolFacts='';
 
-$("#dogeDiv").draggable();
+
+  coolFacts = nameLength(userName);
+  coolFacts += "<h2>" + nameStart(userName) + "</h2>";
+  coolFacts += "<h2>" + nameEnd(userName) + "</h2>";
+  coolFacts += "<h2>" + nameReverse(userName) + "</h2>";
+
+  return coolFacts;
+}
+
+function nameLength(name){
+  return "Your name is " + name.length + " letters long";
+}
+
+function nameStart(name){
+  return "The First letter is " + name.charAt(0);
+}
+
+function nameEnd(name){
+  let lastIndex = name.length - 1
+  return "The Last letter of your name is " + name[lastIndex]
+}
+
+function nameReverse(name){
+  return "I put my thing down, flip it and reverse it: " + name.split("").reverse().join("");
+}
+
+
+
+//$ = jquery's way of naming stuff
+//$(...) = what's inside () is the "object" we are messing with
+// .ready() = method of $jQ
+// .ready(...) = argument/parameter passed to that method
+// function(){} in the method is an IIFE = Immediate Invoked F/n Expression
 
 
   // let userGreeting = "You are enough, ";
